2375f213f782aea853af99f907cf92f28223315e
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2020-03-11 07:38:33 +0800

    FEATURE: a page for each action list

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,23 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = async function({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allAirtable {
+        edges {
+          node {
+            data {
+              Name
+            }
+          }
+        }
+      }
+    }
+  `)
+  data.allAirtable.edges.forEach(edge => {
+    const slug = edge.node.data.Name
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/templates/action.js`),
+      context: { slug: slug },
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,83 +5,81 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-class IndexPage extends Component{
-
-  state = { 
-    query: ''
+class IndexPage extends Component {
+  state = {
+    query: "",
   }
 
-  queryHandler = query => { 
-  this.setState({
-    query: query
-  })
-
-  }
-  
-  clearFilterHandler =() => { 
+  queryHandler = query => {
     this.setState({
-      query: ''
+      query: query,
     })
-    
   }
 
-  render () { 
-
-  const onboardingTasks = this.props.data.allAirtable.nodes
-  const { query } = this.state
-
-  console.log(this.state);
-  console.log(onboardingTasks);
-  
-  const filteredTasks =
-  query === ""
-    ? onboardingTasks
-    : onboardingTasks.filter(c =>
-        c.data.Name.toLowerCase().includes(query.toLowerCase())
-      );
-
-      
-
-  
-  
-
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <h1>Integrating Airtable with Gatsby</h1>
-      <div
-        style={{
-          maxWidth: `300px`,
-          marginBottom: `1.45rem`,
-        }}
-      >
-        <Image />
-      </div>
-      <input 
-        type="text"
-        value={query}
-        placeholder="search your task..." 
-        style={{padding: `10px`, border: `1px solid pink`, width: `100%`, marginBottom: `40px`}}
-        onChange={(e) => this.queryHandler(e.target.value)}/>
-      {filteredTasks.map(task => {
-        return (
-          <div
-            style={{
-              padding: `10px`,
-              border: `1px solid gray`,
-              marginBottom: `10px`,
-            }}
-            key={task.id}
-          >
-            {task.data.Name}
-          </div>
-        )
-      })}
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
-  )
+  clearFilterHandler = () => {
+    this.setState({
+      query: "",
+    })
   }
-  
+
+  render() {
+    const onboardingTasks = this.props.data.allAirtable.nodes
+    const { query } = this.state
+
+    console.log(this.props)
+
+    console.log(this.state)
+    console.log(onboardingTasks)
+
+    const filteredTasks =
+      query === ""
+        ? onboardingTasks
+        : onboardingTasks.filter(c =>
+            c.data.Name.toLowerCase().includes(query.toLowerCase())
+          )
+
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>Integrating Airtable with Gatsby</h1>
+        <div
+          style={{
+            maxWidth: `300px`,
+            marginBottom: `1.45rem`,
+          }}
+        >
+          <Image />
+        </div>
+        <input
+          type="text"
+          value={query}
+          placeholder="search your task..."
+          style={{
+            padding: `10px`,
+            border: `1px solid pink`,
+            width: `100%`,
+            marginBottom: `40px`,
+          }}
+          onChange={e => this.queryHandler(e.target.value)}
+        />
+        {filteredTasks.map(task => {
+          return (
+            <div
+              style={{
+                padding: `10px`,
+                border: `1px solid gray`,
+                marginBottom: `10px`,
+              }}
+              key={task.id}
+            >
+              {task.data.Name}
+            </div>
+          )
+        })}
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
 }
 
 export const actionList = graphql`
